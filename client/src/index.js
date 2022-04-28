cfbe08b62555534e82a5d60251e76d4cf3f923c2
Suraj Shende <surajshende247@gmail.com>
2022-04-28 10:14:12 +0530

    update to new React18 root API

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,12 @@
 // bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import React from "react";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
