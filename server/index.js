c09a2c2ad877cd66bcecde5a63cc1f4b1d76f041
Suraj Shende <surajshende247@gmail.com>
2022-10-03 15:19:19 +0530

    ⚙️ migrate CommonJS to EsModule

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const path = require('path'); 
-const mongoose = require('mongoose')
+import express from 'express';
+import dotennv from 'dotenv';
+import path from 'path';
+import mongoose from 'mongoose';
 
+dotennv.config();
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  console.log('Connected to DB 📦');
-});
+try{
+  mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('Connected to DB 📦');
+  });
+}catch(err){
+  console.log(`❌ Error:  ${err?.message}`);
+}
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
@@ -20,6 +24,13 @@ if(process.env.NODE_ENV === 'production') {
       res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
   });
 }
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    message: 'Server is running'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT} 🚀`);
